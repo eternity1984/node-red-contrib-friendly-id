47f4a5a5ba6edf9372f836cb54d20900ece4c160
yu_inoue <eternity.1984.10.04@gmail.com>
2021-02-07 19:48:49 +0900

    🚨  fix linter warning

--- node/friendlyid.js
+++ node/friendlyid.js
@@ -1,126 +1,119 @@
-module.exports = function(RED) {
-    'use strict'
-    const makeAction = require('./lib/actions')
-    const makeStatus = require('./lib/status')
+module.exports = function (RED) {
+  'use strict'
+  const makeAction = require('./lib/actions')
+  const makeStatus = require('./lib/status')
 
-    class FriendlyIdNode {
-        constructor(config) {
-            this.config = config;
-            this.nodeStatus = makeStatus(this, config)
-            RED.nodes.createNode(this, config)
+  class FriendlyIdNode {
+    constructor (config) {
+      this.config = config
+      this.nodeStatus = makeStatus(this, config)
+      RED.nodes.createNode(this, config)
 
-            this.nodeStatus.init()
+      this.nodeStatus.init()
 
-            this.on("input", this.onInput.bind(this));
-            this.on('close', this.onClose.bind(this));
+      this.on('input', this.onInput.bind(this))
+      this.on('close', this.onClose.bind(this))
+    }
+
+    onClose () {
+      this.nodeStatus.close()
+    }
+
+    onInput (msg, send, done) {
+      this.request(msg, (data, err) => {
+        if (err) {
+          this.onError(msg, send, done, err)
+        } else {
+          this.onSuccess(msg, send, done, data)
         }
+      })
+    }
 
-        onClose() {
-            this.nodeStatus.close()
-        }
+    onSuccess (msg, send, done, data) {
+      // For maximum backwards compatibility, check that send exists.
+      // If this node is installed in Node-RED 0.x, it will need to
+      // fallback to using `node.send`
+      send = send || function () { this.send.apply(this, arguments) }
 
-        onInput(msg, send, done) {
-            this.request(msg, (data, err) => {
-                if (err) {
-                    this.onError(msg, send, done, err)
-                } else {
-                    this.onSuccess(msg, send, done, data)
-                }
-            })
-        }
+      this.setMessageProperty(msg, this.config, data)
+      send(msg)
+      if (done) {
+        done()
+      }
+      const text = this.getStatusMessage(msg, this.config, data)
+      this.nodeStatus.success(text)
+    }
 
-        onSuccess(msg, send, done, data) {
-            // For maximum backwards compatibility, check that send exists.
-            // If this node is installed in Node-RED 0.x, it will need to
-            // fallback to using `node.send`
-            send = send || function() { this.send.apply(this, arguments) }
+    onError (msg, send, done, err) {
+      if (done) {
+        done(err)
+      } else {
+        this.error(err, msg)
+      }
+      this.nodeStatus.error(err)
+    }
 
-            this.setMessageProperty(msg, this.config, data)
-            send(msg)
-            if (done) {
-                done()
-            }
-            let text = this.getStatusMessage(msg, this.config, data)
-            this.nodeStatus.success(text)
-        }
-
-        onError(msg, send, done, err) {
-            if (done) {
-                done(err)
-            } else {
-                this.error(err, msg)
-            }
-            this.nodeStatus.error(err)
-        }
-
-        /**
-         * 
-         * @param {*} msg 
-         * @param {(data, err) => void} callback 
+    /**
+         *
+         * @param {*} msg
+         * @param {(data, err) => void} callback
          */
-        request(msg, callback) {
-            try {
-                let action = makeAction(this.config)
-                if (action === undefined) {
-                    throw Error(RED._('label.mode.undefined'))
-                }
-                let data = action.exec(this.getMessageProperty(msg, this.config))
-                callback(data, null)
-
-            } catch (e) {
-                callback(null, e)
-            }
+    request (msg, callback) {
+      try {
+        const action = makeAction(this.config)
+        if (action === undefined) {
+          throw Error(RED._('label.mode.undefined'))
         }
+        const data = action.exec(this.getMessageProperty(msg, this.config))
+        callback(data, null)
+      } catch (e) {
+        callback(null, e)
+      }
+    }
 
-        getStatusMessage(msg, config, data) {
-            var output = ''
-            if (config.statusType === 'auto') {
-                output = data
-            } else if (config.statusType === 'msg') {
-                output = RED.util.getMessageProperty(msg, config.statusVal)
-            }
-            return output
-        }
+    getStatusMessage (msg, config, data) {
+      var output = ''
+      if (config.statusType === 'auto') {
+        output = data
+      } else if (config.statusType === 'msg') {
+        output = RED.util.getMessageProperty(msg, config.statusVal)
+      }
+      return output
+    }
 
-        /**
-         * 
-         * @param {Object} msg 
-         * 		the message object
-         * @param {Object} config 
-         * 		the config object
+    /**
+         *
+         * @param {Object} msg the message object
+         * @param {Object} config the config object
          * @returns {String}
          */
-        getMessageProperty(msg, config) {
-            let prop = "payload" // auto
-            if (config.inputFromType === 'msg') {
-                prop = config.inputFromVal
-            }
-            let input = RED.util.getMessageProperty(msg, prop)
-            if (input === undefined) {
-                throw Error('Missing input property: msg.' + target)
-            }
-            return input
-        }
-
-        /**
-         * 
-         * @param {Object} msg	
-         * 		the message object
-         * @param {Object} config
-         * 		the config object
-         * @param {*} value
-         * 		the value to set
-         */
-        setMessageProperty(msg, config, value) {
-            let prop = "payload" // auto
-            if ((config.outputToType === 'inplace') && (config.inputFromType === 'msg')) {
-                prop = config.inputFromVal
-            } else if (config.outputToType === 'msg') {
-                prop = config.outputToVal
-            }
-            RED.util.setMessageProperty(msg, prop, value, true)
-        }
-
+    getMessageProperty (msg, config) {
+      let prop = 'payload' // auto
+      if (config.inputFromType === 'msg') {
+        prop = config.inputFromVal
+      }
+      const input = RED.util.getMessageProperty(msg, prop)
+      if (input === undefined) {
+        throw Error('Missing input property: msg.' + prop)
+      }
+      return input
     }
-    RED.nodes.registerType('friendly-id', FriendlyIdNode)
+
+    /**
+         *
+         * @param {Object} msg the message object
+         * @param {Object} config the config object
+         * @param {*} value the value to set
+         */
+    setMessageProperty (msg, config, value) {
+      let prop = 'payload' // auto
+      if ((config.outputToType === 'inplace') && (config.inputFromType === 'msg')) {
+        prop = config.inputFromVal
+      } else if (config.outputToType === 'msg') {
+        prop = config.outputToVal
+      }
+      RED.util.setMessageProperty(msg, prop, value, true)
+    }
+  }
+  RED.nodes.registerType('friendly-id', FriendlyIdNode)
 }
--- node/lib/status.js
+++ node/lib/status.js
@@ -1,69 +1,67 @@
 /**
- * 
- * @param {*} node 
+ *
+ * @param {*} node
  * @param {Object} config
- * @returns {NodeStatus} 
+ * @returns {NodeStatus}
  */
-module.exports = function(node, config) {
-    return new NodeStatus((config.tostatus) ? new ActiveReporter(node) : new Reporter())
+module.exports = function (node, config) {
+  return new NodeStatus((config.tostatus) ? new ActiveReporter(node) : new Reporter())
 }
 
 class NodeStatus {
+  constructor (reporter) {
+    this.reporter = reporter
+  }
 
-    constructor(reporter) {
-        this.reporter = reporter
-    }
+  init () {
+    this.reporter.init()
+  }
 
-    init() {
-        this.reporter.init()
-    }
+  close () {
+    this.reporter.close()
+  }
 
-    close() {
-        this.reporter.close()
-    }
+  success (text) {
+    this.reporter.success(text)
+  }
 
-    success(text) {
-        this.reporter.success(text)
-    }
-
-    error(text) {
-        this.reporter.error(text)
-    }
+  error (text) {
+    this.reporter.error(text)
+  }
 }
 
 class Reporter {
+  init () {}
 
-    init() {}
+  close () {}
 
-    close() {}
+  success (text) {}
 
-    success(text) {}
-
-    error(text) {}
+  error (text) {}
 }
 
 class ActiveReporter extends Reporter {
-    constructor(node) {
-        super()
-        this.node = node
-    }
+  constructor (node) {
+    super()
+    this.node = node
+  }
 
-    init() {
-        this.node.status({ fill: 'grey', shape: 'ring' })
-    }
+  init () {
+    this.node.status({ fill: 'grey', shape: 'ring' })
+  }
 
-    close() {
-        this.node.status({})
-    }
+  close () {
+    this.node.status({})
+  }
 
-    success(text) {
-        if (text.length > 64) {
-            text = text.substr(0, 64) + '...'
-        }
-        this.node.status({ fill: 'grey', shape: 'dot', text: text })
+  success (text) {
+    if (text.length > 64) {
+      text = text.substr(0, 64) + '...'
     }
+    this.node.status({ fill: 'grey', shape: 'dot', text: text })
+  }
 
-    error(text) {
-        this.node.status({ fill: 'red', shape: 'dot', text: 'Failed' })
-    }
+  error (text) {
+    this.node.status({ fill: 'red', shape: 'dot', text: 'Failed' })
+  }
 }
